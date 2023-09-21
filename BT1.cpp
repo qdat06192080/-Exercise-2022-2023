0d53e523088c59b7dd0e4cbf880e0e695162031c
qdat06192080 <145685037+qdat06192080@users.noreply.github.com>
2023-09-21 20:53:51 +0700

    Add files via upload

---
+++ BT1.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+int b[1000];
+
+void sinh(set<int> a){
+    int j=0;
+     for (auto i: a){
+        b[j]=i;
+        j++;
+     }
+}
+
+int main(){
+  freopen("BT.INP","r",stdin);
+  freopen("BT.OUT","w",stdout);
+  ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+  int n,k;
+  cin >> n >> k;
+  set<int> a;
+  for (int i=0;i<=n;i++){
+     int c;
+     cin >> c;
+     a.insert(c);
+  }
+  sinh(a);
+  int j=a.size()-1;
+  int kq=b[j];
+  int x=1;
+  if (kq==k){
+    x=2;
+  }
+  while (x!=2){
+       kq=kq+b[j-1];
+       if (kq>k){
+         kq=kq-b[j-1];
+         j--;
+       } else x++;
+
+  }
+  cout<<kq;
+  return 0;
+}
+
+
