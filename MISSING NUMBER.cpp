82329c2dce63186d5ccd0de9042f9e1ff3b22642
V0Q6D <145685037+qdat06192080@users.noreply.github.com>
2023-12-22 21:41:05 +0700

    Add files via upload

---
+++ MISSING NUMBER.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main(){
+   freopen("BT.INP","r",stdin);
+   freopen("BT.OUT","w",stdout);
+   int n;
+   cin >> n;
+   n--;
+   long long a[n];
+   for (int i=0;i<=n;i++){
+       cin >> a[i];
+    }
+   sort(a,a+n);
+   for (int i=0;i<=n;i++){
+      if (a[i]!=i+1){
+        cout<<i+1;
+        break;
+      }
+    }
+   return 0;
+}
+
+
+//https://cses.fi/problemset/task/1083/
